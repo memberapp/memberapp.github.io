fbecb39f22037c588dc130692c94d8437968cdf7
FreeTrade <FreeTrade@users.noreply.github.com>
2019-10-13 09:36:06 +0800

    Change to dev server

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 "use strict";
-var server="https://memberjs.org/v2/member.php";
-var graphserver="https://memberjs.org/v2/trustgraph.php";
+var server="https://memberjs.org/dev/member.php";
+var graphserver="https://memberjs.org/dev/trustgraph.php";
  
