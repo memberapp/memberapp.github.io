dd015c755ef04c530d16a7b31f36972da3e82453
FreeTrade <FreeTrade@users.noreply.github.com>
2019-10-28 12:50:09 +0800

    Use main server

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var server="https://memberjs.org/dev/member.php";
-var graphserver="https://memberjs.org/dev/trustgraph.php";
+var server="https://memberjs.org/v2/member.php";
+var graphserver="https://memberjs.org/v2/trustgraph.php";
 
 utxoServer = "https://rest.bitcoin.com/v2/";
 txbroadcastServer = "https://memberjs.org:8123/v2/";
