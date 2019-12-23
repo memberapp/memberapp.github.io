abb1b19dcf747e8804eef53582f2f01d0e658ec6
2qx <2qx@small.neomailbox.ch>
2019-12-23 05:01:14 -0500

    restrict language string input, map zh locales

--- js/loadlocale.js
+++ js/loadlocale.js
@@ -1,11 +1,27 @@
 (function () {
     'use strict';
-
-    var language = (window.navigator.language || window.browserLanguage || window.userLanguage).toLowerCase();
-    [
-        // a short substring is used here in order to
-        language.substring(0,3).split('-')[0]
-    ].forEach(function (src) {
+    const zhTraditional = ["zh-hk", "zh-tw", "zh-hant"];
+    const zhSimplified = ["zh-cn", "zh-sg", "zh-hans"];
+    const allowedInput = ["af", "an", "ar", "as", "azb", "az", "bal", "bel",
+        "bg", "bn", "bo", "bs", "ca", "ckb", "cs", "cy", "da", "de", "el",
+        "en", "eo", "es", "et", "eu", "fa", "fi", "fo", "fr", "fy", "ga", "gd",
+        "gl", "gu", "haz", "he", "hi", "hr", "hsb", "hu", "id", "is", "it",
+        "ja", "jv", "kab", "ka", "kir", "kk", "km", "kmr", "kn", "ko", "lt",
+        "lv", "me", "mg", "mk", "ml", "mn", "mr", "ms", "mya", "nb", "ne",
+        "nl", "nn", "os", "pa", "pl", "ps", "pt", "ro", "ru", "si", "sk",
+        "skr", "sl", "snd", "so", "sq", "sr", "sv", "sw", "szl", "ta", "te",
+        "th", "tl", "tr", "ug", "uk", "ur", "uz", "vi", "zh", "zht"];
+    var language = (window.navigator.language || window.browserLanguage).toLowerCase();
+    if (zhSimplified.includes(language)) {
+        language = 'zh'
+    } else if (zhTraditional.includes(language)) {
+        language = 'zht'
+    } else {
+        language = language.substring(0, 3).split('-')[0]
+    }
+    // As the language is controlled by the users browser,
+    // the input is restricted to a known set of possibilities
+    if allowedInput.includes(language){
         console.log('loading: ' + src);
         let script = document.createElement('script');
         script.setAttribute('type', 'application/json');
@@ -13,7 +29,9 @@
         script.setAttribute('data-vavilon-dict', src);
         script.async = false;
         document.head.appendChild(script);
-    });
+    }else(
+        console.log("refusing to load unsupported language string:" + language)
+    )
 
 }());
 	  
