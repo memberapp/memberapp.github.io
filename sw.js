5f2ab58f56f98b11b0a329db473e18bf92369a9a
2qx <2qx@small.neomailbox.ch>
2019-12-23 09:13:11 -0500

    cache files on sw install

--- sw.js
+++ sw.js
@@ -21,6 +21,16 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener("activate", event => {
     console.log('Activate!');
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', function (event) {
