92caaa1e5abe66c5bb27405137fbee22a4bbd982
FreeTrade <memorycoincc@gmail.com>
2020-01-08 17:44:48 +0700

    try to remove old sw.js

--- pwa/pwa.js
+++ pwa/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('sw.js').then(function (registration) {
+        navigator.serviceWorker.register('sw.js?version=3.5.2').then(function (registration) {
             // Registration was successful
             console.log('Registered!');
         }, function (err) {
