b4de0f2a609ad1d1a92fb1cfa5d96dcde74e35c3
2qx <2qx@small.neomailbox.ch>
2020-01-09 19:30:40 -0500

    register sw

--- pwa/pwa.js
+++ pwa/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        /*navigator.serviceWorker.register('sw.js?version=3.5.2').then(function (registration) {
+        navigator.serviceWorker.register('sw.js?version=3.5.2').then(function (registration) {
             // Registration was successful
             console.log('Registered!');
         }, function (err) {
@@ -8,7 +8,7 @@ if ('serviceWorker' in navigator) {
             console.log('ServiceWorker registration failed: ', err);
         }).catch(function (err) {
             console.log(err);
-        });*/
+        });
         navigator.serviceWorker.getRegistrations().then(
 
             function(registrations) {
