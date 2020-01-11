24bbb596a16a2707cdd66417ec0056e4b1713c7b
2qx <2qx@small.neomailbox.ch>
2020-01-11 11:44:03 -0500

    test version upgrade

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const PRECACHE_URLS = [
     'js/leaflet/leaflet.js',
     'locale/en.json'
 ];
-const VERSION = '3.5.2.9';
+const VERSION = '3.5.2.12';
 const RUNTIME = 'runtime-' + VERSION;
 const INSTALL = 'install-' + VERSION;
 
