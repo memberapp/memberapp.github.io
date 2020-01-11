b3a947cb3ee69542494dbf3b0381566f100691ed
2qx <2qx@small.neomailbox.ch>
2020-01-11 14:22:52 -0500

    switch to offical spec manifest extension

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // A list of local resources we always want to be cached.
 
 const PRECACHE_URLS = [
-    'pwa/manifest.json',
+    'pwa/manifest.webmanifest',
     'css/article.css',
     'css/base.css',
     'img/bch.png',
