41cd2d0db5e9c6097d21526b998fc6308cc24796
F <>
2020-08-12 18:22:50 +0100

    .

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const PRECACHE_URLS = [
 ];
 
 //If updating version here, also update version in login.js
-const version = "4.0.34";
+const version = "4.0.35";
 
 const RUNTIME = "runtime-" + version;
 const INSTALL = "install-" + version;
