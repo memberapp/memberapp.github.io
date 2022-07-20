98e1f19d9bbe8f8c26863503c51e896c907fb7fa
FreeTrade <memorycoincc@gmail.com>
2022-07-20 09:04:15 +0700

    8.2.3 bump version

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const PRECACHE_URLS = [
 ];
 
 //If updating version here, also update version in login.js
-const version = '8.2.2';
+const version = '8.2.3';
 
 const RUNTIME = 'runtime-' + version;
 const INSTALL = 'install-' + version;
